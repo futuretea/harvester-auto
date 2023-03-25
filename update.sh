088d96838e14adbff598b8d4158f0c28be053fc6
futuretea <1913508671@qq.com>
2023-03-25 22:48:33 +0800

    add update.sh

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+usage() {
+    cat <<HELP
+USAGE:
+    update.sh 
+HELP
+}
+
+git pull
+make
+sudo systemctl stop harvester-auto
+mv ./bin/harvester-auto .
+sudo systemctl start harvester-auto
