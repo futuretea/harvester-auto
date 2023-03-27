fbda5eba1cac54feb34143ff6147e30d53eaefff
futuretea <1913508671@qq.com>
2023-03-27 12:17:51 +0800

    Update url command
    Signed-off-by: futuretea <1913508671@qq.com>

--- commands/url.sh
+++ commands/url.sh
@@ -22,21 +22,31 @@ source _config.sh
 workspace_cluster="${workspace_root}/${cluster_name}"
 workspace="${workspace_cluster}/harvester-auto"
 
+host_ip=$(hostname -I | awk '{print $1}')
+echo "Host Nginx URL: http://${host_ip}"
+
 harvester_mgmt_url_file="${workspace}/harvester_mgmt_url.txt"
 if [[ -f "${harvester_mgmt_url_file}" ]]; then
+  echo "Socks5 Proxy URL: socks5://${host_ip}:1080"
 
   harvester_mgmt_url=$(cat "${harvester_mgmt_url_file}")
-  echo "harvester mgmt url: ${harvester_mgmt_url}"
+  echo "Harvester Management URL(need proxy): ${harvester_mgmt_url}"
 
   harvester_explorer_url=${harvester_mgmt_url}/dashboard/c/local/explorer
-  echo "harvester explorer url: ${harvester_explorer_url}"
+  echo "Harvester Embedded Rancher URL(need proxy): ${harvester_explorer_url}"
 
   harvester_longhorn_url=${harvester_mgmt_url}/dashboard/c/local/longhorn
-  echo "harvester longhorn url: ${harvester_longhorn_url}"
+  echo "Harvester Longhorn URL(need proxy): ${harvester_longhorn_url}"
 
-  host_ip=$(hostname -I | awk '{print $1}')
-  echo "host nginx url: http://${host_ip}"
-  echo "socks5 proxy url: socks5://${host_ip}:1080"
+  if [ -n "${nfs_root_dir}" ];then
+    nfs_cluster_dir="${nfs_root_dir}/${cluster_name}"
+    if [ ! -d "${nfs_cluster_dir}" ];then
+        mkdir -p "${nfs_cluster_dir}"
+        chown -R nobody:nogroup "${nfs_cluster_dir}"
+        chmod -R 777 "${nfs_cluster_dir}"
+    fi
+    echo "NFS Backup Target URL: nfs://${host_ip}:${nfs_cluster_dir}"
+  fi
 else
   echo "N/A"
 fi
