335ccde536c4fe7bb6ecc1a06ee5bf8e2dd25fd8
futuretea <1913508671@qq.com>
2023-03-27 16:28:48 +0800

    url: support read from settings

--- commands/kubeconfig.sh
+++ commands/kubeconfig.sh
@@ -29,10 +29,11 @@ fi
 
 # get kubeconfig from the first node
 first_node_ip="10.${user_id}.${cluster_id}.11"
-sshpass -p "${default_node_password}" ssh -tt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no rancher@"${first_node_ip}" sudo cat "/etc/rancher/rke2/rke2.yaml" > ${kubeconfig_file} 2>/dev/null
+sshpass -p "${default_node_password}" ssh -tt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no rancher@"${first_node_ip}" sudo cat "/etc/rancher/rke2/rke2.yaml" > "${kubeconfig_file}.src" 2>/dev/null
+cat "${kubeconfig_file}.src" | yq e '.clusters[0].cluster.server = "https://'"${first_node_ip}"':6443"' - >"${kubeconfig_file}"
 
 # update kubeconfig server and proxy-url
 host_ip=$(hostname -I | awk '{print $1}')
 socks5_ip=${host_ip}
 socks5_port=1080
-cat "${kubeconfig_file}" | yq e '.clusters[0].cluster.server = "https://'"${first_node_ip}"':6443"' - | yq e '.clusters[0].cluster.proxy-url = "socks5://'"${socks5_ip}:${socks5_port}"'"' -
+cat "${kubeconfig_file}" | yq e '.clusters[0].cluster.proxy-url = "socks5://'"${socks5_ip}:${socks5_port}"'"' -
