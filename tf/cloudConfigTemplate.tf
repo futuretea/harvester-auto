3704a98e60f557116e1a1c2809f577a1c69773ab
futuretea <1913508671@qq.com>
2023-03-28 15:29:36 +0800

    Update cloudConfigTemplate
    Signed-off-by: futuretea <1913508671@qq.com>

--- tf/cloudConfigTemplate.tf
+++ tf/cloudConfigTemplate.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_config_map" "password" {
   metadata {
     name      = "password"
-    namespace = "default"
+    namespace = "harvester-public"
     labels = {
       "harvesterhci.io/cloud-init-template" : "user"
     }
@@ -14,6 +14,37 @@ resource "kubernetes_config_map" "password" {
       chpasswd:
         expire: false
       ssh_pwauth: true
+      EOF
+  }
+}
+
+resource "kubernetes_config_map" "docker-rancher" {
+  metadata {
+    name      = "docker-rancher"
+    namespace = "harvester-public"
+    labels = {
+      "harvesterhci.io/cloud-init-template" : "user"
+    }
+  }
+
+  data = {
+    cloudInit = <<-EOF
+      #cloud-config
+      password: password
+      chpasswd:
+        expire: false
+      ssh_pwauth: true
+      package_update: true
+      packages:
+        - qemu-guest-agent
+      runcmd:
+        - - systemctl
+          - enable
+          - --now
+          - qemu-guest-agent.service
+        - "curl -sL https://releases.rancher.com/install-docker/20.10.sh | bash -"
+        - "sudo systemctl enable --now docker"
+        - "docker run -itd --name rancher --privileged=true  --restart=unless-stopped -p 443:443 -p 80:80 rancher/rancher:v2.7-head"
       EOF
   }
 }
