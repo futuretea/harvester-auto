68d185f6602b6553fc827b9812d5fb3bb7764593
futuretea <1913508671@qq.com>
2023-04-01 17:11:19 +0800

    Add clean
    Signed-off-by: futuretea <1913508671@qq.com>

---
+++ commands/clean.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+usage() {
+    cat <<HELP
+USAGE:
+    clean.sh 
+HELP
+}
+
+if [ $# -lt 0 ]; then
+    usage
+    exit 1
+fi
+
+docker images | grep harvester | grep -v none | awk '{print "docker rmi "$1":"$2}' | bash
+docker system prune -f
+docker volume prune -f
+docker image prune -f
+rm -rf /var/www/html/harvester/*
