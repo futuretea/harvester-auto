ae148519b80a30d3726f04c5094fda059bbdd80a
futuretea <1913508671@qq.com>
2023-04-15 14:18:46 +0800

    update status
    Signed-off-by: futuretea <1913508671@qq.com>

--- commands/status.sh
+++ commands/status.sh
@@ -19,7 +19,17 @@ cluster_id=$2
 cluster_name="harvester-${namespace_id}-${cluster_id}"
 
 source _config.sh
+source _util.sh
+host_ip=$(get_host_ip)
+echo "Cluster        Node        State        Display"
+echo "-------------------------------------------------"
 
-set +e
-sudo virsh list --all | grep "harvester-auto_${cluster_name}"
-set -e
+for vm in $(sudo virsh -q list --all | awk '{print $2}'); do
+  if [[ ! "${vm}" =~ harvester-auto_${cluster_name} ]]; then
+    continue
+  fi
+  vm_no_prefix="${vm#harvester-auto_${cluster_name}-}"
+  port_number=$(sudo virsh vncdisplay "${vm}" | awk -F ":" '{print $2}')
+  state=$(sudo virsh domstate "${vm}")
+  echo "${cluster_id}   ${vm_no_prefix}   ${state}   vnc://${host_ip}:${port_number}"
+done
