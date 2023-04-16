0b9f7dc1b8f06c55cf424d3b27c2a353a6037ea0
futuretea <1913508671@qq.com>
2023-04-16 13:51:11 +0800

    fix status
    Signed-off-by: futuretea <1913508671@qq.com>

--- commands/status.sh
+++ commands/status.sh
@@ -21,18 +21,25 @@ cluster_name="harvester-${namespace_id}-${cluster_id}"
 source _config.sh
 source _util.sh
 host_ip=$(get_host_ip)
-echo "Cluster        Node        State        Display"
+
+echo "Cluster        Node        State        Console"
 echo "-------------------------------------------------"
 
-for vm in $(sudo virsh -q list --all | awk '{print $2}'); do
-  if [[ ! "${vm}" =~ harvester-auto_${cluster_name} ]]; then
+for vm_name in $(sudo virsh -q list --all | awk '{print $2}'); do
+  vm="${vm_name#harvester-auto_}"
+  if [[ ! "${vm}" == ${cluster_name}* ]]; then
     continue
   fi
-  vm_no_prefix="${vm#harvester-auto_${cluster_name}-}"
-  vnc_port_suffix=$(sudo virsh vncdisplay "${vm}" | awk -F ":" '{print $2}')
-  vnc_port=$((5900+${vnc_port_suffix}))
-  novnc_port=$((6080+${vnc_port_suffix}))
-  sudo snap set novnc services.n${novnc_port}.listen=${novnc_port} services.n${novnc_port}.vnc=localhost:${vnc_port}
-  state=$(sudo virsh domstate "${vm}")
-  echo "${cluster_id}   ${vm_no_prefix}   ${state}   http://${host_ip}:${novnc_port}/vnc.html"
+
+  node_index="${vm#${cluster_name}-}"
+  state=$(sudo virsh domstate "${vm_name}")
+  printf "%s   %s   %s   " "${cluster_id}" "${node_index}" "${state}"
+
+  if [ "${state}" == "running" ]; then
+    novnc_port=$(get_vm_novnc_port "${vm_name}")
+    echo "http://${host_ip}:${novnc_port}/vnc.html"
+  else
+    echo "N/A"
+  fi
+
 done
