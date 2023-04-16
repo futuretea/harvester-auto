7d13e76a65d9753a09b719008346a836941c0b1c
futuretea <1913508671@qq.com>
2023-04-16 16:08:59 +0800

    update l
    Signed-off-by: futuretea <1913508671@qq.com>

--- commands/l.sh
+++ commands/l.sh
@@ -17,13 +17,38 @@ fi
 namespace_id=$1
 
 source _config.sh
+source _util.sh
+host_ip=$(get_host_ip)
 
-echo "ID NAME"
+echo "Id        Name        URL        State        Console"
+echo "--------------------------------------------------"
 for folder in "${workspace_root}"/*; do
-  if [ -d "$folder" ] && [[ "$folder" == "${workspace_root}/harvester-${namespace_id}-"* ]]; then
-    awk -F "-" '{printf $3"  "}' <<<"$folder"
+  if [ -d "${folder}" ] && [[ "${folder}" == "${workspace_root}/harvester-${namespace_id}-"* ]]; then
+    cluster_name=${folder#${workspace_root}/}
+    cluster_id=$(awk -F "-" '{printf $3}' <<<"${cluster_name}")
+
+    # cluster name
+    name="N/A"
     if [ -f "${folder}/_name" ]; then
-      cat "${folder}/_name"
+      name=$(cat "${folder}/_name")
+    fi
+
+    # cluster url
+    workspace_cluster="${workspace_root}/${cluster_name}"
+    workspace="${workspace_cluster}/harvester-auto"
+    url="N/A"
+    harvester_mgmt_url_file="${workspace}/harvester_mgmt_url.txt"
+    if [[ -f "${harvester_mgmt_url_file}" ]]; then
+      url=$(cat "${harvester_mgmt_url_file}")
+    fi
+
+    # cluster state
+    first_node_name="harvester-auto_${cluster_name}-1"
+    state=$(sudo virsh domstate "${first_node_name}")
+    printf "%s        %s        %s        %s        " "${cluster_id}" "${name}" "${url}" "${state}"
+    if [ "${state}" == "running" ]; then
+      novnc_port=$(get_vm_novnc_port "${first_node_name}")
+      echo "http://${host_ip}:${novnc_port}/vnc.html"
     else
       echo "N/A"
     fi
