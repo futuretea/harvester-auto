b6be580133f2ba986499c2189d35522ea1874281
futuretea <1913508671@qq.com>
2023-04-19 11:58:27 +0800

    Support image url config
    Signed-off-by: futuretea <1913508671@qq.com>

--- tf/image.tf
+++ tf/image.tf
@@ -4,5 +4,5 @@ resource "harvester_image" "focal-server" {
 
   display_name = "focal-server-cloudimg-amd64.img"
   source_type  = "download"
-  url          = "http://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
+  url          = "${var.image_url}"
 }
---
+++ tf/variables.tf
@@ -0,0 +1,3 @@
+variable "image_url" {
+  type        = string
+}
