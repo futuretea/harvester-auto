7e57ef307cef843ab2420bb19896d1da75bb6e78
futuretea <1913508671@qq.com>
2023-06-08 23:05:28 +0800

    Support build rancher dashboard
    Signed-off-by: futuretea <1913508671@qq.com>

--- commands/_ui_config.sh
+++ commands/_ui_config.sh
@@ -6,6 +6,10 @@ export ui_bucket_name="harvester-dev"
 export ui_dashboard_bucket_dir="bot-dashboard"
 export ui_dashboard_output_target="oss://${ui_bucket_name}/${ui_dashboard_bucket_dir}"
 export ui_dashboard_base_url="https://${ui_bucket_name}.${ui_bucket_endpoint}/${ui_dashboard_bucket_dir}"
+# rancher dashboard
+export ui_rancher_dashboard_bucket_dir="bot-rancher-dashboard"
+export ui_rancher_dashboard_output_target="oss://${ui_bucket_name}/${ui_rancher_dashboard_bucket_dir}"
+export ui_rancher_dashboard_base_url="https://${ui_bucket_name}.${ui_bucket_endpoint}/${ui_rancher_dashboard_bucket_dir}"
 # plugin
 export ui_plugin_bucket_dir="bot-plugin"
 export ui_plugin_output_target="oss://${ui_bucket_name}/${ui_plugin_bucket_dir}"
