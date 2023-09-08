9222e0e604a01c9b8f3f1bb3a1e49da9e25d02ae
futuretea <1913508671@qq.com>
2023-09-08 11:41:39 +0800

    fix harvester_image
    Signed-off-by: futuretea <1913508671@qq.com>

--- terraform/image.tf
+++ terraform/image.tf
@@ -1,8 +1,17 @@
-resource "harvester_image" "focal-server" {
-  name      = "focal-server"
+resource "harvester_image" "focal-server-replicas-1" {
+  name      = "focal-server-replicas-1"
   namespace = "harvester-public"
 
   storage_class_name = harvester_storageclass.any-replicas-1.name
+
+  display_name = "focal-server-cloudimg-amd64-replicas-1.img"
+  source_type  = "download"
+  url          = var.ubuntu_image_url
+}
+
+resource "harvester_image" "focal-server" {
+  name      = "focal-server"
+  namespace = "harvester-public"
 
   display_name = "focal-server-cloudimg-amd64.img"
   source_type  = "download"
